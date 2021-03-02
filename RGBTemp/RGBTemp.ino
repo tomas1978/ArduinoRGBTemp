d2967076087d0cbd9fbc4803f8c4b1fb080e8dd7
tomas1978 <tomas.nilsson78@gmail.com>
2021-03-02 17:25:00 +0100

    Added functionality
    Added code that shows red light if temperature > 25 degC, else the led shows blue light.

--- RGBTemp/RGBTemp.ino
+++ RGBTemp/RGBTemp.ino
@@ -7,18 +7,28 @@ void setup()
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  
+  Serial.begin(9600);
 }
 
 float sensorToTemp(int analogPort) {
-    int rawAnalogValue = analogRead(analogPort);
-    float volt = (float)rawAnalogValue * 5.0 / 1023.0;
-    float degC = (volt-0.5) * 100.0;
-    return degC;
+  int rawAnalogValue = analogRead(analogPort);
+  float volt = (float)rawAnalogValue * 5.0 / 1023.0;
+  float degC = (volt-0.5) * 100.0;
+  return degC;
 }
 
 void loop()
 {
-    float temperature=sensorToTemp(0);
-    Serial.println(temperature);
+  digitalWrite(greenPin,LOW);
+  float temperature=sensorToTemp(0);
+  if(temperature>25) {
+    digitalWrite(redPin,HIGH);
+    digitalWrite(bluePin,LOW);
+  }
+  else {
+    digitalWrite(redPin,LOW);
+    digitalWrite(bluePin,HIGH);
+  }
+  Serial.println(temperature);  
+  delay(10);
 }
